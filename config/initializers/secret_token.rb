0caa99e68dc2a42189273b2805907c7d77d88eb3
larsburgess <larsburgess@gmail.com>
2012-05-22 18:06:50 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventtracker::Application.config.secret_token = '3189f88c22273cdd279694c67ea50cdc621c136aede6230dd3777e42d1dbbbbe663d6578d5191f38b8b83e9ecf88e88ca59589016c9e03b32724f85df09cf5d9'
