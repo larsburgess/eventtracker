29145789720c8e190ca3020a5aecc1b102caa288
larsburgess <larsburgess@gmail.com>
2012-10-10 17:07:14 -0700

    small change for uri

--- config/initializers/rhoconnect.rb
+++ config/initializers/rhoconnect.rb
@@ -1,6 +1,6 @@
 Rhoconnect.configure do |config|
 
-	config.uri          = "http://90f99da064374193b1eb185e112324e7@localhost:9292"
+	#config.uri          = "http://90f99da064374193b1eb185e112324e7@localhost:9292"
 	config.app_endpoint = "http://localhost:3000"
 	#config.app_endpoint  = "http://rho-fat-free.heroku.com"
 	config.authenticate = lambda { |credentials| true }
